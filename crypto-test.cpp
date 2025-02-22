ef5f47d851a128a847e3c7052dd23569f393cc76
imseano <soreta@csu.fullerton.edu>
2025-02-21 22:30:47 -0800

    Establish IO
    Switch namespace and char* to string
    
    Establish file verification
    
    Line to string
    
    IO skeleton for encrypt and decrypt algorithms

--- crypto-test.cpp
+++ crypto-test.cpp
@@ -1,29 +1,60 @@
 #include <iostream>
+#include <fstream>
 
-char* encrypt(char* message);
-char* decrypt(char* message);
+using namespace std;
+string encrypt(string message);
+string decrypt(string message);
 
 int main() {
-        std::cout << "Initializing Cryptography Test..." << std::endl << "hi" << std::endl;
+	fstream file;
+	string filename;
+	string input, output;
 
-	char* message = "Hello this is a test";
-	std::cout << message << std::endl;
+        cout << "Initializing Cryptography Test..." << endl << "hi" << endl;
 
-	char* encrypted = encrypt(message);
-        std::cout << encrypted << std::endl;
+	string message = "Please input the name of the file to be encrypted: ";
+	cout << message << endl;
+	cin >> filename;
 
-	char* decrypted = decrypt(encrypted);
-	std::cout << decrypted << std::endl;
+	file.open(filename, ios::in);
+        if (!file) {
+                cout << "File not found." << endl;
+                return 1;
+	}
+
+	string line;
+
+	while (getline(file, line)) {
+		input += line + "\n";
+        }
+
+	cout << input << endl;
+
+	string encrypted = encrypt(input);
+        cout << "Your encrypted message is: " << encrypted << endl;
+
+	string decrypted = decrypt(encrypted);
+	cout << "Your decrypted message is: " << decrypted << endl;
+
+	if (encrypted == decrypted) {
+                cout << "Success!" << endl;
+        }
 
         return 0;
 }
 
-char* encrypt(char* message) {
-	std::cout << "I will encrypt the message" << std::endl;
-	return message;
+string encrypt(string message) {
+	string encryption = message;
+	ofstream file("encrypted.txt");
+	cout << "I will encrypt the message" << endl;
+	file << encryption;
+	return encryption;
 }
 
-char* decrypt(char* message) {
-        std::cout << "I will decrypt the message" << std::endl;
-        return message;
+string decrypt(string message) {
+	string decryption = message;
+	ofstream file("decrypted.txt");
+        cout << "I will decrypt the message" << endl;
+	file << decryption;
+        return decryption;
 }
